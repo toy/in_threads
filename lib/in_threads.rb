ebf1eb8255b1aab9414c336cd36f231cfbc48897
Ivan Kuchin <tadump+git@gmail.com>
2011-12-05 02:48:58 +0100

    don't use method_missing, directly specify methods to run with run_in_threads_consecutive (updated its code from method_missing)

--- lib/in_threads.rb
+++ lib/in_threads.rb
@@ -27,6 +27,20 @@ class InThreads
     self.class.new(@enumerable, thread_count, &block)
   end
 
+  %w[
+    each
+    all? any? none? one?
+    detect find find_index drop_while take_while
+    partition find_all select reject count
+    collect map group_by max_by min_by minmax_by sort_by
+  ].each do |name|
+    class_eval <<-RUBY
+      def #{name}(*args, &block)
+        run_in_threads_consecutive(:#{name}, *args, &block)
+      end
+    RUBY
+  end
+
   def reverse_each(*args, &block)
     @enumerable.reverse_each(*args, &block)
   end
@@ -63,14 +77,6 @@ class InThreads
     RUBY
   end
 
-  def method_missing(method, *args, &block)
-    if block
-      run_in_threads_consecutive(method, *args, &block)
-    else
-      @enumerable.send(method, *args)
-    end
-  end
-
 private
 
   def run_in_threads_block_result_irrelevant(method, *args, &block)
@@ -90,31 +96,37 @@ private
   end
 
   def run_in_threads_consecutive(method, *args, &block)
-    queue = Queue.new
-    runner = Thread.new do
-      threads = []
+    if block
       begin
-        @enumerable.each do |object|
-          if threads.length >= thread_count
-            threads = threads.select(&:alive?)
-            if threads.length >= thread_count
-              ThreadsWait.new(*threads).next_wait
+        queue = Queue.new
+        runner = Thread.new do
+          threads = []
+          begin
+            @enumerable.each do |object|
+              if threads.length >= thread_count
+                threads = threads.select(&:alive?)
+                if threads.length >= thread_count
+                  ThreadsWait.new(*threads).next_wait
+                end
+              end
+              break if Thread.current[:stop]
+              thread = Thread.new(object, &block)
+              threads << thread
+              queue << thread
             end
+          ensure
+            threads.map(&:join)
           end
-          break if Thread.current[:stop]
-          thread = Thread.new(object, &block)
-          threads << thread
-          queue << thread
+        end
+        @enumerable.send(method, *args) do |*block_args|
+          queue.pop.value
         end
       ensure
-        threads.map(&:join)
+        runner[:stop] = true
+        runner.join
       end
+    else
+      @enumerable.send(method, *args)
     end
-    @enumerable.send(method, *args) do |*block_args|
-      queue.pop.value
-    end
-  ensure
-    runner[:stop] = true
-    runner.join
   end
 end
